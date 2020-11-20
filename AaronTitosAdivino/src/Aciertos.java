8078fac909560f8910950eaf7df05fd0a6470d89
Vicente <vicantser@yahoo.es>
2020-11-20 22:41:37 +0100

    Corregido

--- AaronTitosAdivino/src/Aciertos.java
+++ AaronTitosAdivino/src/Aciertos.java
@@ -1,10 +1,12 @@
+//Corregido por Vicente
 import java.util.Scanner;
 
 public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (Math.random()*(10-50+1)+50);
+        //int x = (int) (Math.random()*(10-50+1)+50); Esta instrucción no incluye ni al 10 ni al 50.
+        int x = (int) (Math.random()*((50-10+1)+10));// Esta si.
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
